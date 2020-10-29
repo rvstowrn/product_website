7dba462ad4623a73fe2ef807b70b44a11cdc0773
Rishabh Verma <rvstowrn@gmail.com>
2020-10-29 21:12:27 +0530

    second commit

--- index.php
+++ index.php
@@ -3,7 +3,7 @@
   <head>
     <meta charset="UTF-8" />
     <meta name="viewport" content="width=device-width, initial-scale=1.0" />
-    <title>LAND fast | Welcome</title>
+    <title>LAND fast | Home</title>
     <meta
       name="description"
       content="Welcome to the best product landing page around."
